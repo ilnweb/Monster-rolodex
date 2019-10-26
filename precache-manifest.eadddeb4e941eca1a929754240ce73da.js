202511a1dc2ef1ba146d76ef71634f85d9513082
iliyan <iliyan.tsachev@ilnweb.com>
2019-10-26 11:32:09 +0200

    Updates

---
+++ precache-manifest.eadddeb4e941eca1a929754240ce73da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8f8e0f3a3a77125b71243f40bfae70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef01eb41c9243fa6803",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6646ef72db40f7b423ff",
+    "url": "/static/js/2.51dd13e8.chunk.js"
+  },
+  {
+    "revision": "7ef01eb41c9243fa6803",
+    "url": "/static/js/main.ad962edd.chunk.js"
+  },
+  {
+    "revision": "a6fff7cf82a6cf270dfb",
+    "url": "/static/js/runtime-main.d7516561.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
